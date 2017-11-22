41c0d3c154717ad2a583fb58c216f3d2706e8306
Luiz Henrique <Luiz Henrique@LUIZ-HENRIQUE>
2017-11-22 14:04:59 -0200

    Criada tela de consulta de ponto

---
+++ MeuPonto/MeuPonto/Model/Ponto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPonto.Model
+{
+    public class Ponto
+    {
+        public DateTime DiaPonto { get; set; }
+        public TimeSpan HoraPonto { get; set; }
+
+        public Ponto()
+        {
+
+        }
+
+        public Ponto(DateTime DiaPontoP, TimeSpan HoraPontoP)
+        {
+            this.DiaPonto = DiaPontoP;
+            this.HoraPonto = HoraPontoP;
+        }
+    }
+}
---
+++ MeuPonto/MeuPonto/View/Consulta.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MeuPonto.Model;
+
+namespace MeuPonto.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Consulta : ContentPage
+    {
+        public Consulta()
+        {
+
+            Ponto[] listaPontos = new Ponto[5];
+            listaPontos[0] = new Ponto(DateTime.Today, new TimeSpan());
+            listaPontos[1] = new Ponto(DateTime.Today, new TimeSpan());
+            listaPontos[2] = new Ponto(DateTime.Today, new TimeSpan());
+            listaPontos[3] = new Ponto(DateTime.Today, new TimeSpan());
+            listaPontos[4] = new Ponto(DateTime.Today, new TimeSpan());
+
+            string[] mensagem = new string[listaPontos.Length];
+            for (int i = 0; i < listaPontos.Length; i++)
+            {
+                mensagem[i] = string.Concat(listaPontos[i].DiaPonto.ToString("dd/MM/yyyy"), " - ", listaPontos[i].HoraPonto.ToString());
+            }
+
+            InitializeComponent();
+            this.ListaPontos.ItemsSource = mensagem;
+        }
+    }
+}
