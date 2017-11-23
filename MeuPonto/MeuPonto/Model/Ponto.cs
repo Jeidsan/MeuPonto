ed43513bf2001f7170d049ffdd1ef37a31f9b40b
Luiz Henrique <Luiz Henrique@LUIZ-HENRIQUE>
2017-11-23 00:15:09 -0200

    Modificações inserção de ponto manual

--- MeuPonto/MeuPonto/Model/Ponto.cs
+++ MeuPonto/MeuPonto/Model/Ponto.cs
@@ -9,26 +9,6 @@ namespace MeuPonto.Model
     public class Ponto : DomainObject
     {
         public DateTime Data { get; set; }
-        //public TimeSpan InicioTrabalho { get; set; }
-        //public TimeSpan InicioAlmoco { get; set; }
-        //public TimeSpan TerminoAlmoco { get; set; }
-        //public TimeSpan TerminoTrabalho { get; set; }
 
-        public Ponto()
-        {
-
-        }
-        public Ponto(DateTime DataP)
-        {
-            this.Data = DataP;
-        }
-        //public Ponto(DateTime DataP, TimeSpan InicioTrabalhoP, TimeSpan InicioAlmocoP, TimeSpan TerminoAlmocoP, TimeSpan TerminoTrabalhoP)
-        //{
-        //    this.Data = DataP;
-        //    this.InicioTrabalho = InicioTrabalhoP;
-        //    this.InicioAlmoco = InicioAlmocoP;
-        //    this.TerminoAlmoco = TerminoAlmocoP;
-        //    this.TerminoTrabalho = TerminoTrabalhoP;
-        //}
     }
 }
